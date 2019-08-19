66e828640cebba96b8b066551c1f88ae65aef492
mattshardman <mattshardman@gmail.com>
2019-08-19 13:53:47 +0100

    remove button

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,10 @@
 import React from "react";
-import styled from 'styled-components';
 import Counter from "../components/Counter";
-import Button from "../components/~common/Button";
-
 
 function Home() {
   return (
     <div>
      <Counter />
-     <Button outline onClick={() => console.log('hello')}>Hello</Button>
     </div>
   );
 }
