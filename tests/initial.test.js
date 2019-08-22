4d8bab964f46f571bfd10ca00e058ab88994e3e6
mattshardman <mattshardman@gmail.com>
2019-08-22 11:26:25 +0100

    set up circle ci script and config

--- tests/initial.test.js
+++ tests/initial.test.js
@@ -2,7 +2,6 @@ describe('Test to check testing environment is setup correctly', () => {
   it('Runs a test correctly', () => {
     const sum = (a, b) => a + b;
     const result = sum(3, 4);
-
     expect(result).toBe(7);
   });
 });
