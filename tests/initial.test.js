e9658847819b1dd66b6c1f68643aea85ebaeb45d
mattshardman <mattshardman@gmail.com>
2019-08-22 11:23:26 +0100

    set up testing install jest

---
+++ tests/initial.test.js
@@ -0,0 +1,8 @@
+describe('Test to check testing environment is setup correctly', () => {
+  it('Runs a test correctly', () => {
+    const sum = (a, b) => a + b;
+    const result = sum(3, 4);
+
+    expect(result).toBe(7);
+  });
+});
