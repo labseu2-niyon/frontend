2d1aebc223c4125c6fac90a3b913b959b47feef5
mattshardman <mattshardman@gmail.com>
2019-08-23 10:21:27 +0100

    chore: add theme provider, update app.js and

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,19 @@
 import App from 'next/app';
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import { Provider } from 'react-redux';
 
 import withReduxStore from '../lib/withReduxStore';
+import { theme } from '../lib/theme';
 
 class MyApp extends App {
   render() {
     const { Component, pageProps, reduxStore } = this.props;
     return (
       <Provider store={reduxStore}>
-        <Component {...pageProps} />
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
       </Provider>
     );
   }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
-import Counter from '../components/Counter';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
+    height: 40px;
+    width: 150px;
+    background: ${({ theme }) => theme.secondary};
+    color: #fff;
+    border: none;
+    border-radius: 4px;
+    box-shadow: ${({ theme }) => theme.boxShadow};
+    cursor: pointer;
+    font-size: 14px;
+`;
 
 function Home() {
   return (
-    <div>
-      <Counter />
-    </div>
+    <Wrapper>
+      <h1>Niyon homepage</h1>
+      <Button>Let&apos;s go</Button>
+    </Wrapper>
   );
 }
 
