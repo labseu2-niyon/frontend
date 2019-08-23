64d7612317f4265f2d1dc38a8025a7b3e1190664
Sorin Chis <kish.sorin@yahoo.com>
2019-08-23 15:28:04 +0300

    added Index.js

---
+++ components/~common/index.js
@@ -0,0 +1,31 @@
+import Button from './Button';
+import Heading from './Headings/Heading';
+import Heading2 from './Headings/Heading2';
+import Heading3 from './Headings/Heading3';
+import Heading4 from './Headings/Heading4';
+import Checkbox from './Checkbox';
+import Divider from './Divider';
+import Dropdown from './Dropdown';
+import ProgressBar from './ProgressBar';
+import SearchBar from './SearchBar';
+import Skip from './Skip';
+import Spinner from './Spinner';
+import TeamCard from './TeamCard';
+import UserCard from './UserCard';
+
+export {
+  Button,
+  Heading,
+  Heading2,
+  Heading3,
+  Heading4,
+  Checkbox,
+  Divider,
+  Dropdown,
+  ProgressBar,
+  SearchBar,
+  Skip,
+  Spinner,
+  TeamCard,
+  UserCard,
+};
