0929ff8188eb9796354d080e533d793e989098dc
krikaz <pascal.bou@protonmail.com>
2019-08-23 17:01:53 +0200

    added diff sizes to Divider comp

--- components/~common/Divider.jsx
+++ components/~common/Divider.jsx
@@ -2,15 +2,24 @@ import React from 'react';
 import styled from 'styled-components';
 
 const D = styled.hr`
-	width: 90%;
+  width: ${({ widthSize }) => widthSize || '90%'};
 	color: #f7f7f7;
 	align-content: center;
 `;
 
 function Divider(props) {
-	const { customStyles } = props;
+	const { customStyles, full, half, short } = props;
 
-	return <D {...props} {...customStyles} />;
+	let size;
+  if (short) {
+    size = '25%';
+  } else if (half) {
+    size = '50%';
+  } else if (full) {
+    size = '100%';
+  }
+
+	return <D widthSize={size} {...props} {...customStyles} />;
 }
 
 export default Divider;
