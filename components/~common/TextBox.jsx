756cc21c912d495b2b82aee96f45e51a4e2cd6af
krikaz <pascal.bou@protonmail.com>
2019-08-23 18:06:49 +0200

    added sizes to textbox comp

--- components/~common/TextBox.jsx
+++ components/~common/TextBox.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const IBox = styled.textarea`
-	max-width: 100%;
+	width: ${({ widthSize }) => widthSize || '20%'};
 	max-height: 100%;
 	border-radius: 10px;
 	padding: 0.5rem;
@@ -10,9 +10,28 @@ const IBox = styled.textarea`
 `;
 
 function TextBox(props) {
-  const { customStyles, form } = props;
+	const { customStyles, form, short, half, long } = props;
 
-	return <IBox placeholder='text box' form={form} {...props} {...customStyles} />;
+	let size;
+  if (short) {
+    size = '10%';
+  } else if (half) {
+    size = '50%';
+  } else if (long) {
+    size = '90%';
+	}
+	
+	let rows = '5';
+	if (short) {
+    rows = '3';
+  } else if (half) {
+    rows = '10';
+  } else if (long) {
+    rows = '20';
+	}
+
+
+	return <IBox placeholder='text box' widthSize={size} rows={rows} form={form} {...props} {...customStyles} />;
 }
 
 export default TextBox;
