3c12689af1a9b118428b56a9af0b2e6e787bf3c8
mattshardman <mattshardman@gmail.com>
2019-09-02 10:23:05 +0100

    chore: create basic layout for profile section

---
+++ components/profile/Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.main`
+    width: 100%;
+    border-top: 1px solid #eaeaea;
+    border-bottom: 1px solid #eaeaea;
+    padding: 2rem 0;
+`;
+
+function Profile() {
+  return (
+    <Container>
+        hello
+    </Container>
+  );
+}
+
+export default Profile;
--- pages/profile.js
+++ pages/profile.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import withAuth from '../lib/withAuth';
 import Layout from '../components/Layout';
+import Profile from '../components/profile/Profile';
 
 
-function Profile() {
+function ProfilePage() {
   return (
-    <Layout pageName="PROFILE" />
+    <Layout pageName="PROFILE">
+      <Profile />
+    </Layout>
   );
 }
 
-export default withAuth(Profile);
+export default withAuth(ProfilePage);
