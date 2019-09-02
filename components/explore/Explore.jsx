a5ae3f351d0dea2216205ddf48aa5c6e2f68012d
mattshardman <mattshardman@gmail.com>
2019-09-02 12:11:34 +0100

    chore: import components to explore component

--- components/explore/Explore.jsx
+++ components/explore/Explore.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
+import TopSection from './TopSection';
+import SearchBox from './SearchBox';
+import ProfileList from './ProfileList';
 
 const Wrapper = styled.main`
     width: 100%;
-    background: crimson;
 `;
 
 function Explore() {
   return (
-    <Wrapper>hi</Wrapper>
+    <Wrapper>
+      <TopSection />
+      <SearchBox />
+      <ProfileList />
+    </Wrapper>
   );
 }
 
