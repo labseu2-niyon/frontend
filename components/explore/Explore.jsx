a9e8f7c34ddab9f6df9da9fce205b53627f68933
mattshardman <mattshardman@gmail.com>
2019-09-02 13:00:41 +0100

    chore: move user and job titles object

--- components/explore/Explore.jsx
+++ components/explore/Explore.jsx
@@ -8,14 +8,42 @@ const Wrapper = styled.main`
     width: 100%;
 `;
 
-const jobTitles = [{ value: 'hello', label: 'hi' }, { value: 'Bonjour', label: 'salut' }];
+const jobTitles = [
+  { value: 'hello', label: 'hi' },
+  { value: 'Bonjour', label: 'salut' },
+];
+
+const users = [{
+  name: 'Andy',
+  field: 'Whaler',
+  description: 'I\'m andy the whaler',
+  src: '',
+  position: 'Whaley Whaler',
+  location: 'Japan',
+},
+{
+  name: 'Bob',
+  field: 'Porker',
+  description: 'I\'m andy the whaler',
+  src: '',
+  position: 'Whaley Whaler',
+  location: 'Japan',
+},
+{
+  name: 'Reggie',
+  field: 'Fisherman',
+  description: 'I\'m andy the whaler',
+  src: '',
+  position: 'Fishy Fisher',
+  location: 'France',
+}];
 
 function Explore() {
   return (
     <Wrapper>
       <TopSection />
       <SearchBox jobTitles={jobTitles} />
-      <ProfileList />
+      <ProfileList users={users} />
     </Wrapper>
   );
 }
