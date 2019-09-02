be3811ede33c4531f9bc2cf7bfd79e537ae63d62
mattshardman <mattshardman@gmail.com>
2019-09-02 15:33:48 +0100

    chore: inject props from query string

--- pages/profile.js
+++ pages/profile.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { withRouter } from 'next/router';
 import withAuth from '../lib/withAuth';
 import Layout from '../components/Layout';
 import Profile from '../components/profile/Profile';
 
-
-function ProfilePage() {
+function ProfilePage(props) {
+  // eslint-disable-next-line
+  const userInfo = props.router.query;
   return (
     <Layout pageName="PROFILE">
-      <Profile />
+      <Profile {...userInfo} />
     </Layout>
   );
 }
 
-export default withAuth(ProfilePage);
+export default withAuth(withRouter(ProfilePage));
