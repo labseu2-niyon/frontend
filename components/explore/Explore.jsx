00f68fc99fc396a3bd165fb523268062d1f1ffaa
mattshardman <mattshardman@gmail.com>
2019-09-02 11:31:53 +0100

    chore: create explore component

---
+++ components/explore/Explore.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.main`
+    width: 100%;
+    background: crimson;
+`;
+
+function Explore() {
+  return (
+    <Wrapper>hi</Wrapper>
+  );
+}
+
+export default Explore;
