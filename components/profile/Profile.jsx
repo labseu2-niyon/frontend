3c20d4e940e60daaf32287b2294b58e10f365106
mattshardman <mattshardman@gmail.com>
2019-09-02 16:47:13 +0100

    get photo src from url

--- components/profile/Profile.jsx
+++ components/profile/Profile.jsx
@@ -7,20 +7,18 @@ const Container = styled.main`
     width: 100%;
 `;
 
-const photoData = {
-  src: 'https://milan.serverlessdays.io/speakers/guillermo-rauch.jpg',
-};
 
 const bioData = {
   position: 'Mentor',
   location: 'San Fransisco',
-  text: ['Lorem ipsum dolor sit amet consectetur adipisicing elit.', 'Veniam aliquam similique ipsa omnis commodi, consequuntur assumenda quos porro eligendi, in odio mollitia optio doloremque laborum vitae, obcaecati quo ratione culpa.'],
+  text: ['Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    'Veniam aliquam similique ipsa omnis commodi, consequuntur assumenda quos porro eligendi, in odio mollitia optio doloremque laborum vitae, obcaecati quo ratione culpa.'],
 };
 
 function Profile(props) {
   return (
     <Container>
-      <PhotoSection {...photoData} {...props} />
+      <PhotoSection {...props} />
       <Bio {...bioData} />
     </Container>
   );
