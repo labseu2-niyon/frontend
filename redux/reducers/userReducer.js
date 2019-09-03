86099d47c612a63667c0a668263a9273d28b3f3c
krikaz <pascal.bou@protonmail.com>
2019-09-03 10:23:18 +0200

    added reducers on correct file

--- redux/reducers/userReducer.js
+++ redux/reducers/userReducer.js
@@ -1,97 +1,106 @@
-// export default (state, action) => {
-//   switch (action.type) {
-//     case actionTypes.UPDATE_USER_PROFILE_REQUEST:
-//       return {
-//         ...state,
-//         queryingDatabase: true
-//       };
-//     case actionTypes.UPDATE_USER_PROFILE_SUCCESS:
-//       return {
-//         ...state,
-//         queryingDatabase: false,
-//         user: [...state.user, action.payload]
-//         // message: action.payload,
-//       };
-//     case actionTypes.UPDATE_USER_PROFILE_FAILURE:
-//       return {
-//         ...state,
-//         queryingDatabase: false,
-//         errorMessage: action.payload
-//       };
+import { types } from '../userConstants';
 
-//     case actionTypes.UPDATE_PASSWORD_REQUEST:
-//       return {
-//         ...state,
-//         queryingDatabase: true
-//       };
-//     case actionTypes.UPDATE_PASSWORD_SUCCESS:
-//       return {
-//         ...state,
-//         queryingDatabase: false,
-//         message: action.payload
-//       };
-//     case actionTypes.UPDATE_PASSWORD_FAILURE:
-//       return {
-//         ...state,
-//         queryingDatabase: false,
-//         errorMessage: action.payload
-//       };
+const initialState = {
+  loading: false,
+  error: null,
+  message: '',
+};
 
-//     case actionTypes.UPLOAD_USER_IMAGE_REQUEST:
-//       return {
-//         ...state,
-//         queryingDatabase: true
-//       };
-//     case actionTypes.UPLOAD_USER_IMAGE_SUCCESS:
-//       return {
-//         ...state,
-//         queryingDatabase: false,
-//         user: [...state.user, action.payload]
-//       };
-//     case actionTypes.UPLOAD_USER_IMAGE_FAILURE:
-//       return {
-//         ...state,
-//         queryingDatabase: false,
-//         errorMessage: action.payload
-//       };
+export const userReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case types.UPDATE_PASSWORD_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case types.UPDATE_PASSWORD_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        message: payload,
+      };
+    case types.UPDATE_PASSWORD_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: payload,
+      };
 
-//     case actionTypes.FETCH_ALL_USERS_REQUEST:
-//       return {
-//         ...state,
-//         queryingDatabase: true
-//       };
-//     case actionTypes.FETCH_ALL_USERS_SUCCESS:
-//       return {
-//         ...state,
-//         queryingDatabase: false,
-//         usersAll: [...state.usersAll, action.payload]
-//       };
-//     case actionTypes.FETCH_ALL_USERS_FAILURE:
-//       return {
-//         ...state,
-//         queryingDatabase: false,
-//         errorMessage: action.payload
-//       };
+    default:
+      return state;
+  }
+};
 
-//     case actionTypes.CHECK_USER_PROFILE_REQUEST:
-//       return {
-//         ...state,
-//         queryingDatabase: true
-//       };
-//     case actionTypes.CHECK_USER_PROFILE_SUCCESS:
-//       return {
-//         ...state,
-//         queryingDatabase: false,
-//         user: [...state.user, action.payload]
-//       };
-//     case actionTypes.CHECK_USER_PROFILE_FAILURE:
-//       return {
-//         ...state,
-//         checkingUserInfo: false,
-//         errorMessage: action.payload
-//       };
+// old reducers
 
-//     default:
-//       return state;
-//   }
-// };
+// case types.UPDATE_USER_PROFILE_REQUEST:
+//   return {
+//     ...state,
+//     loading: true,
+//   };
+// case types.UPDATE_USER_PROFILE_SUCCESS:
+//   return {
+//     ...state,
+//     loading: false,
+//     user: [...state.user, payload],
+//   };
+// case types.UPDATE_USER_PROFILE_FAILURE:
+//   return {
+//     ...state,
+//     loading: false,
+//     error: payload,
+//   };
+
+// case types.UPLOAD_USER_IMAGE_REQUEST:
+//   return {
+//     ...state,
+//     loading: true,
+//   };
+// case types.UPLOAD_USER_IMAGE_SUCCESS:
+//   return {
+//     ...state,
+//     loading: false,
+//     user: [...state.user, payload],
+//   };
+// case types.UPLOAD_USER_IMAGE_FAILURE:
+//   return {
+//     ...state,
+//     loading: false,
+//     error: payload,
+//   };
+
+// case types.FETCH_ALL_USERS_REQUEST:
+//   return {
+//     ...state,
+//     loading: true,
+//   };
+// case types.FETCH_ALL_USERS_SUCCESS:
+//   return {
+//     ...state,
+//     loading: false,
+//     usersAll: [...state.usersAll, payload],
+//   };
+// case types.FETCH_ALL_USERS_FAILURE:
+//   return {
+//     ...state,
+//     loading: false,
+//     error: payload,
+//   };
+
+// case types.CHECK_USER_PROFILE_REQUEST:
+//   return {
+//     ...state,
+//     loading: true,
+//   };
+// case types.CHECK_USER_PROFILE_SUCCESS:
+//   return {
+//     ...state,
+//     loading: false,
+//     user: [...state.user, payload],
+//   };
+// case types.CHECK_USER_PROFILE_FAILURE:
+//   return {
+//     ...state,
+//     checkingUserInfo: false,
+//     error: payload,
+//   };
