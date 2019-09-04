c7ba7cee3c7bf0afedc4677ce9dd7f528aa633ad
krikaz <pascal.bou@protonmail.com>
2019-09-04 10:14:14 +0200

    corrected action creator

--- redux/actions/authActions.js
+++ redux/actions/authActions.js
@@ -165,44 +165,20 @@ export const logOutUser = () => (dispatch) => {
   nookies.destroy({}, 'token', { path: '/' });
 };
 
-// export const registerUser = newUser => dispatch => {
-//   dispatch({ type: actionTypes.REGISTER_USER_REQUEST });
-//   // spinner
-//   axios
-//     .post(`${_BASE_URL}/signup`, newUser)
-//     .then(res => {
-//       dispatch({
-//         type: actionTypes.REGISTER_USER_SUCCESS,
-//         payload: res.data,
-//         // payload: res.data.token,
-//       });
-//       // window.localStorage.setItem('token', res.data.token);
-//       // window.location = '/user/dashboard';
-//     })
-//     .catch(error => {
-//       dispatch({
-//         type: actionTypes.REGISTER_USER_FAILURE,
-//         payload: error.message,
-//       });
-//     });
-// };
-
-// export const resetPassword = email => dispatch => {
-//   dispatch({ type: actionTypes.RESET_PASSWORD_REQUEST });
-//   // spinner
-//   axios
-//     .post(`${_BASE_URL}/resetpassword`, email)
-//     .then(res => {
-//       dispatch({
-//         type: actionTypes.RESET_PASSWORD_SUCCESS,
-//         payload: res.data,
-//       });
-//       // window.location = '/auth/email-sent';
-//     })
-//     .catch(error => {
-//       dispatch({
-//         type: actionTypes.RESET_PASSWORD_FAILURE,
-//         payload: error.message,
-//       });
-//     });
-// };
+export const resetPassword = (email) => (dispatch) => {
+  dispatch({ type: types.RESET_PASSWORD_REQUEST });
+  axios
+    .post(`${_BASE_URL}/user/resetpassword`, email)
+    .then((res) => {
+      dispatch({
+        type: types.RESET_PASSWORD_SUCCESS,
+        payload: res.data.data.message,
+      });
+    })
+    .catch((error) => {
+      dispatch({
+        type: types.RESET_PASSWORD_FAILURE,
+        payload: error.message,
+      });
+    });
+};
