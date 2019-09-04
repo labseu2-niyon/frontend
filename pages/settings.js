f3a683fa591d52a8da3baa60a7a745821c88d530
Delba <delba.brown@gmail.com>
2019-09-04 12:42:01 +0100

    Fetch initial props for settings page

--- pages/settings.js
+++ pages/settings.js
@@ -1,16 +1,30 @@
+import nookies from 'nookies';
+import jwt from 'jsonwebtoken';
 import Layout from '../components/Layout';
 import withAuth from '../lib/withAuth';
 import Wrapper from '../components/settings/Wrapper';
 import EditProfile from '../components/settings/EditProfile';
+import axiosWithToken from '../redux/axios';
 
-function Page() {
+function Page({ user }) {
   return (
     <Layout pageName="Settings">
       <Wrapper>
-        <EditProfile />
+        <EditProfile user={user} />
       </Wrapper>
     </Layout>
   );
 }
+
+Page.getInitialProps = async (ctx) => {
+  const cookies = nookies.get(ctx);
+  const { username } = jwt.decode(cookies.token);
+
+  const res = await axiosWithToken(ctx).get(
+    `https://niyon-dev.herokuapp.com/api/user/${username}/profile`,
+  );
+
+  return { user: res.data.data };
+};
 
 export default withAuth(Page);
