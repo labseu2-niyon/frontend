f9e2a322142bde61a708a393936c0da59d668b8c
krikaz <pascal.bou@protonmail.com>
2019-09-04 13:31:04 +0200

    created new password page

---
+++ pages/auth/new-password.js
@@ -0,0 +1,10 @@
+import NewPassword from '../../components/auth/NewPassword';
+
+const newPassword = () => (
+  <div>
+    <p>Navbar from Marketing</p>
+    <NewPassword />
+  </div>
+);
+
+export default newPassword;
