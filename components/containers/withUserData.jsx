05a8fd8f4ed2cbf41f93e69896db2ef351a4ccf5
mattshardman <mattshardman@gmail.com>
2019-09-05 12:17:00 +0100

    chore: create logic for container HOC

--- components/containers/withUserData.jsx
+++ components/containers/withUserData.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { fetchAllUsers, fetchAllConnections } from '../../redux/actions/userActions';
+
 
 const withUserData = (Component) => {
   function WithUserData(props) {
-    return <Component {...props} />;
+    const [users, setUsers] = useState([]);
+
+    const mapUsers = (input) => {
+      const withDisplay = input.map((user) => ({ ...user, display: true }));
+      setUsers(withDisplay);
+    };
+
+    useEffect(() => {
+      (async () => {
+        if (!props.usersAll) {
+          await props.fetchUsers();
+        }
+        mapUsers(props.usersAll);
+      })();
+
+      if (!props.connectionsAll) {
+        props.fetchConnections();
+      }
+    }, []);
+
+    return <Component {...props} users={users} />;
   }
 
-  return WithUserData;
+  const mapStateToProps = (state) => state.userReducer;
+
+  const mapDispatchToProps = (dispatch) => dispatch({
+    fetchUsers: fetchAllUsers,
+    fetchConnections: fetchAllConnections,
+  });
+
+  WithUserData.propTypes = {
+    usersAll: PropTypes.arrayOf().isRequired,
+    connectionsAll: PropTypes.arrayOf().isRequired,
+    fetchUsers: PropTypes.func.isRequired,
+    fetchConnections: PropTypes.func.isRequired,
+  };
+
+  return connect(mapStateToProps, mapDispatchToProps)(WithUserData);
 };
 
 export default withUserData;
