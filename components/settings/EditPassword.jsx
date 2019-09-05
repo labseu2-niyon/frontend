a8decb8508d0d5bacce68919db661636d393cab6
Delba <delba.brown@gmail.com>
2019-09-05 12:18:54 +0100

    Add form validation for passwords

--- components/settings/EditPassword.jsx
+++ components/settings/EditPassword.jsx
@@ -1,21 +1,61 @@
 import styled from 'styled-components';
+import { useState } from 'react';
 
 const EditPassword = () => {
+  const [oldPassword, setOldPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [repeatPassword, setRepeatPassword] = useState('');
+  const [error, setError] = useState(false);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const body = {
+      password: oldPassword,
+      newPassword: newPassword
+    };
+
+    if (!error) {
+      updatePassword(body);
+      setOldPassword('');
+      setNewPassword('');
+      setRepeatPassword('');
+    }
+  };
+
+  const checkPassword = () => {
+    if (newPassword !== repeatPassword) {
+      setError(true);
+    } else {
+      setError(false);
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <p>Old Password:</p>
-        <input type="password"></input>
+        <input
+          type="password"
+          onChange={e => setOldPassword(e.target.value)}
+        ></input>
       </div>
       <div>
         <p>New Password:</p>
-        <input type="password"></input>
+        <input
+          type="password"
+          onChange={e => setNewPassword(e.target.value)}
+        ></input>
       </div>
       <div>
         <p>Confirm New Password:</p>
-        <input type="password"></input>
+        <input
+          type="password"
+          onChange={e => setRepeatPassword(e.target.value)}
+          onBlur={checkPassword}
+        ></input>
+        {error ? <p>Please make sure both passwords match</p> : null}
       </div>
-      <button>Save</button>
+      <input type="submit" value="Submit" />
     </form>
   );
 };
