2d9223c2fdf6f34ae2952e95aaf1200ee4139dd5
mattshardman <mattshardman@gmail.com>
2019-09-05 11:18:32 +0100

    feature: get data from api

--- components/home/Home.jsx
+++ components/home/Home.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import TopSection from './TopSection';
 import ProfileList from './ProfileList';
+import { fetchAllUsers, fetchAllConnections } from '../../redux/actions/userActions';
 
 const Wrapper = styled.div`
     width: 100%;
@@ -9,65 +12,37 @@ const Wrapper = styled.div`
     flex-direction: column;
 `;
 
-const profiles = [{
-  name: 'Andy',
-  field: 'Whaler',
-  description: 'I\'m andy the whaler',
-  src: 'https://image.flaticon.com/icons/svg/1167/1167989.svg',
-  position: 'Mentor',
-  location: 'Australia',
-},
-{
-  name: 'Bob',
-  field: 'Banker',
-  description: 'I\'m Bob the Banker',
-  src: 'https://image.flaticon.com/icons/svg/1573/1573412.svg',
-  position: 'Mentor',
-  location: 'Brussels',
-},
-{
-  name: 'Reggie',
-  field: 'Fisherman',
-  description: 'I\'m Reggie the fisherman',
-  src: 'https://image.flaticon.com/icons/svg/307/307911.svg',
-  position: 'Mentee',
-  location: 'France',
-}, {
-  name: 'Alan',
-  field: 'Athlete',
-  description: 'I\'m Alan the athlete',
-  src: 'https://image.flaticon.com/icons/svg/1473/1473225.svg',
-  position: 'Mentee',
-  location: 'France',
-},
-{
-  name: 'Ronald',
-  field: 'Fry Chef',
-  description: 'I\'m Ronald the fry chef',
-  src: 'https://image.flaticon.com/icons/svg/817/817382.svg',
-  position: 'Mentee',
-  location: 'France',
-}, {
-  name: 'Pete',
-  field: 'Guy',
-  description: 'I\'m Pete i\'m just a guy',
-  src: 'https://image.flaticon.com/icons/svg/145/145859.svg',
-  position: 'Mentee',
-  location: 'France',
-}];
+function Home(props) {
+  useEffect(() => {
+    if (!props.usersAll) {
+      props.fetchUsers();
+    }
 
-function Home() {
+    if (!props.connectionsAll) {
+      props.fetchConnections();
+    }
+  }, []);
+
   return (
     <Wrapper>
-      <TopSection />
-      <ProfileList title="UserSuggestions" users={profiles} />
+      <TopSection numOfConnections={props.connectionsAll.length} />
+      <ProfileList title="UserSuggestions" users={props.usersAll || []} />
       <div style={{
         width: '100%', height: 1, background: '#eaeaea', border: 'none',
       }}
       />
-      <ProfileList title="My Connections" users={profiles} />
+      <ProfileList title="My Connections" users={props.connectionsAll || []} />
     </Wrapper>
   );
 }
 
-export default Home;
+const mapStateToProps = (state) => state.userReducer;
+
+Home.propTypes = {
+  fetchUsers: PropTypes.func.isRequired,
+  fetchConnections: PropTypes.func.isRequired,
+  usersAll: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+  connectionsAll: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+};
+
+export default connect(mapStateToProps, { fetchUsers: fetchAllUsers, fetchConnections: fetchAllConnections })(Home);
