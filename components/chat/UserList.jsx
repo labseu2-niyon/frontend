3666ec0cddcf303ea68214ffbab24abcedcc5dca
Sorin Chis <kish.sorin@yahoo.com>
2019-09-14 17:42:34 +0300

    fixed bug on scroll userList

--- components/chat/UserList.jsx
+++ components/chat/UserList.jsx
@@ -1,27 +1,26 @@
 import styled from 'styled-components';
 import User from './User';
-import Scrollbar from './Scrollbar';
+// import Scrollbar from './Scrollbar';
+import { Card } from 'antd';
 
 const UserList = ({ usersList, socket, currentUser }) => {
   return (
     <Root>
-      <Scrollbar style={{ height: '80vh' }}>
-        {usersList.map(user => (
-          <User
-            user={user}
-            key={user.id}
-            socket={socket}
-            currentUser={currentUser}
-          />
-        ))}
-      </Scrollbar>
+      {usersList.map(user => (
+        <User
+          user={user}
+          key={user.id}
+          socket={socket}
+          currentUser={currentUser}
+        />
+      ))}
     </Root>
   );
 };
 
 export default UserList;
 
-const Root = styled.div`
+const Root = styled(Card)`
   width: 135px;
   height: 80vh;
   overflow-y: scroll;
