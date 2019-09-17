0bf61acdeb35f8694fcb43bfc7c695bf7bd08885
Delba <delba.brown@gmail.com>
2019-09-17 15:17:22 +0100

    Format date in Comment comp

--- components/chat/Comment.jsx
+++ components/chat/Comment.jsx
@@ -1,16 +1,19 @@
-import { Comment, Icon, Tooltip, Avatar } from 'antd';
+import { Comment, Tooltip, Avatar } from 'antd';
+const moment = require('moment');
 
-const CommentComp = ({ username, message, date, image }) => (
-  <Comment
-    author={<a>{username}</a>}
-    avatar={<Avatar src={image} alt={username} />}
-    content={<p>{message}</p>}
-    datetime={
-      <Tooltip title="Date in another format">
-        <span>{date}</span>
-      </Tooltip>
-    }
-  />
-);
+const CommentComp = ({ username, message, date, image }) => {
+  return (
+    <Comment
+      author={<a>{username}</a>}
+      avatar={<Avatar src={image} alt={username} />}
+      content={<p>{message}</p>}
+      datetime={
+        <Tooltip title="Date in another format">
+          <p>{moment(date).fromNow()}</p>
+        </Tooltip>
+      }
+    />
+  );
+};
 
 export default CommentComp;
