a08fe23430f597ec41a692db2a992f79e854bdc0
Delba <delba.brown@gmail.com>
2019-09-17 13:20:01 +0100

    Add chatHistory logic (sender / receiver)

--- components/chat/ChatMessage.jsx
+++ components/chat/ChatMessage.jsx
@@ -3,50 +3,29 @@ import styled from 'styled-components';
 import Comment from './Comment';
 
 const ChatMessage = ({ user, currentUser }) => {
-  console.log('CURRETN USER', currentUser.username);
-  if (user.sender.username === currentUser.username) {
-    return (
-      <Sender>
-        <Comment
-          username={user.sender.username}
-          message={user.message}
-          date={user.dateSent}
-          image={user.sender.profile_picture}
-        />
-      </Sender>
-    );
-  }
-  if (user.sender.username !== currentUser.username) {
-    return (
-      <Recevier>
-        <Comment
-          username={user.reciever.username}
-          message={user.message}
-          date={user.dateSent}
-          image={user.reciever.profile_picture}
-        />
-      </Recevier>
-    );
-  }
   return (
-    <div>
-      <p>No Messages</p>
-    </div>
+    <Sender
+      currentUser={user.sender.username === currentUser.username ? true : false}
+    >
+      <Comment
+        username={
+          user.sender.username === currentUser.username
+            ? currentUser.username
+            : user.sender.username
+        }
+        message={user.message}
+        date={user.dateSent}
+        image={user.sender.profile_picture}
+      />
+    </Sender>
   );
 };
 
 export default ChatMessage;
 
-const Recevier = styled.div`
-  background-color: #e6f6fb;
-  display: flex;
-  padding-left: 20px;
-  /* justify-content: flex-start; */
-`;
-
 const Sender = styled.div`
   background-color: #eafaf1;
   display: flex;
   padding-left: 5px;
-  /* justify-content: flex-end; */
+  background-color: ${props => (props.currentUser ? '#eafaf1' : '#e6f6fb')};
 `;
