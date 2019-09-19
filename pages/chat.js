83277080d72803a8802204929445bafb08c58ad2
Delba <delba.brown@gmail.com>
2019-09-19 15:37:11 +0100

    Clean up code - connections are working!

--- pages/chat.js
+++ pages/chat.js
@@ -1,11 +1,17 @@
 import withAuth from '../lib/withAuth';
-import Layout2 from '../components/Layout2';
+import ChatData from '../components/ChatData';
 import ChatLayout from '../components/chat/ChatLayout';
 
 const Page = props => (
-  <Layout2 pageName="Chat">
+  <div>
+    {/* This component is important for getting connections to work, it fetches the
+    user data on the background (not the connections) and saves it to the redux store. I don't really
+    know why it works the way it does... all I know is that it only works a sibling and not a parent component,
+  (it messes up the socket.on event if it's a parent). Some weird Socket.io behaviour for sure.
+    I've had many sleepless nights.   */}
+    <ChatData> </ChatData>
     <ChatLayout socket={props.socket} />
-  </Layout2>
+  </div>
 );
 
 export default withAuth(Page);
