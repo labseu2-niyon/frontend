de1673345349e3fe6697ee5ece5f0adacf6ffabf
Delba <delba.brown@gmail.com>
2019-09-19 16:24:31 +0100

    Add default image for user message

--- components/chat/ChatMessage.jsx
+++ components/chat/ChatMessage.jsx
@@ -10,12 +10,15 @@ const ChatMessage = ({ user, currentUser }) => {
       <Comment
         username={
           user.sender.username === currentUser.username
-            ? currentUser.username
-            : user.sender.username
+            ? `${currentUser.first_name} ${currentUser.last_name}`
+            : `${user.sender.first_name} ${user.sender.last_name}`
         }
         message={user.message}
         date={user.dateSent}
-        image={user.sender.profile_picture}
+        image={
+          user.sender.profile_picture ||
+          'https://image.flaticon.com/icons/svg/660/660611.svg'
+        }
       />
     </Sender>
   );
@@ -24,7 +27,6 @@ const ChatMessage = ({ user, currentUser }) => {
 export default ChatMessage;
 
 const Sender = styled.div`
-  /* background-color: #eafaf1; */
   display: flex;
   padding-left: 5px;
   /* background-color: ${props =>
