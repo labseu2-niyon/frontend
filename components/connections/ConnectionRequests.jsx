9730a0d2f76068d676b12383afc1d5a14190360e
mattshardman <mattshardman@gmail.com>
2019-09-23 12:34:44 +0100

    update connection card styling

--- components/connections/ConnectionRequests.jsx
+++ components/connections/ConnectionRequests.jsx
@@ -2,21 +2,17 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { Icon } from 'antd';
+import Link from 'next/link';
 import { Heading2 } from '../~common';
 import ExploreCard from '../explore/ExploreCard';
 
 const Wrapper = styled.div`
     box-sizing: border-box;
     width: 100%;
-    background: #eaeaea;
     border-radius: 8px;
-    border: #484848 1px solid;
     margin: 1rem 0;
-    padding: 0 16px;
-    max-height: ${({ open }) => (open ? '1200px' : '70px')};
     display: flex;
     flex-direction: column;
-    overflow: hidden;
     transition: max-height 100ms;
 `;
 
@@ -40,21 +36,80 @@ const CardWrapper = styled.div`
     box-sizing: border-box;
     display: flex;
     flex-wrap: wrap;
-    justify-content: space-between;
+    justify-content: flex-start;
+    align-items: flex-end;
+    height: 100px;
     width: 100%;
 `;
 
+const Person = styled.div`
+  box-sizing: border-box;
+  width: auto;
+  height: 60px;
+  border-radius: 40px;
+  background: #eaeaea;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 8px 6px;
+  padding-right: 32px;
+  margin-right: 32px;
+  transition: transform 400ms;
+
+  .img-wrapper {
+    width: 52px;
+    height: 52px;
+    border-radius: 50%;
+    overflow: hidden;
+    margin-right: 8px;
+  }
+
+  .content {
+    display: flex;
+    flex-direction: column;
+  }
+
+  small {
+    color: #484848;
+  }
+
+  p {
+    margin: 5px 0;
+  }
+
+  &:hover {
+    transform: scale(1.05);
+    cursor: pointer;
+  }
+`;
+
 function ConnectionRequests({ users }) {
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(true);
+  console.log(users);
 
   return (
     <Wrapper open={open}>
       <Header open={open}>
         <Heading2>Connection Requests</Heading2>
-        <Icon type="down" className="icon" onClick={() => setOpen(!open)} />
       </Header>
       <CardWrapper>
-        { users.map(({ connection }) => <ExploreCard {...connection} />) }
+        { users.map(({ connection }) => (
+          <Link href={{
+            pathname: '/profile',
+            query: { id: connection.id }
+          }}
+          >
+            <Person>
+              <div className="img-wrapper">
+                <img src={connection.profile_picture || 'https://image.flaticon.com/icons/svg/660/660611.svg'} alt="" height="100%" />
+              </div>
+              <div className="content">
+                <small>{connection.username}</small>
+                <p>{connection.first_name} {connection.last_name}</p>
+              </div>
+            </Person>
+          </Link>
+        )) }
       </CardWrapper>
     </Wrapper>
   );
