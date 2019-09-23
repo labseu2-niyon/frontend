4348f44256a1f6ac0e6ca1aa3af9973537c66800
Delba <delba.brown@gmail.com>
2019-09-23 10:44:33 +0100

    Check for request before using window

--- redux/actions/utils.js
+++ redux/actions/utils.js
@@ -1,18 +1,25 @@
-// const getUrl = () => {
-//   let _BASE_URL;
-//   if (process.env.NODE_ENV === 'production') {
-//     _BASE_URL =
-//       window.location.origin === 'https://app.niyonapp.com'
-//         ? 'https://niyon-prod.herokuapp.com/api'
-//         : 'https://niyon-staging.herokuapp.com/api';
-//   } else {
-//     _BASE_URL = 'https://niyon-dev.herokuapp.com/api';
-//   }
+function getOrigin(req) {
+  var protocol = 'https:';
+  var host = req
+    ? req.headers['x-forwarded-host'] || req.headers['host']
+    : window.location.host;
+  if (host.indexOf('localhost') > -1) {
+    protocol = 'http:';
+  }
+  return protocol + '//' + host;
+}
 
-//   return _BASE_URL;
-// };
+export const getUrl = req => {
+  let _BASE_URL;
+  if (process.env.NODE_ENV === 'production') {
+    const url = getOrigin(req);
+    _BASE_URL =
+      url === 'https://app.niyonapp.com'
+        ? 'https://niyon-prod.herokuapp.com/api'
+        : 'https://niyon-staging.herokuapp.com/api';
+  } else {
+    _BASE_URL = 'https://niyon-dev.herokuapp.com/api';
+  }
 
-export const getUrl = () => {
-  const _BASE_URL = 'https://niyon-dev.herokuapp.com/api';
   return _BASE_URL;
 };
