ef16fd47a4b48dfed60d3f0804ad461851bcb4f9
krikaz <pascal.bou@protonmail.com>
2019-09-24 17:47:12 +0200

    comment test

--- components/chat/ChatLayout.jsx
+++ components/chat/ChatLayout.jsx
@@ -47,19 +47,68 @@ const Main = styled.div`
     margin: 0;
     border: none;
   }
+
+  
 `;
 
-const mapStateToProps = state => {
-  return {
-    currentUser: state.userReducer.user,
-    authReducer: state.authReducer,
-    //current connection its comming dirrectly from withAuth when user loggedin
-    currentConnectionId: state.authReducer.connectionId
-    //- not taken from redux-store anymore
-  };
-};
+const mapStateToProps = state => ({
+  currentUser: state.userReducer.user,
+  authReducer: state.authReducer,
+  // current connection its comming dirrectly from withAuth when user loggedin
+  currentConnectionId: state.authReducer.connectionId
+  // - not taken from redux-store anymore
+});
 
 export default connect(
   mapStateToProps,
   {}
 )(ChatLayout);
+
+
+// @media screen and (max-width: 600px) {
+
+//   .hXtAjt {
+//     display: flex;
+//     justify-content: space-between;
+//   }
+
+//   /* left side with list of users */
+//   .ant-card.jvJjgw {
+//     background-color: green;
+//     width: 15%;
+//   }
+
+//   /* top left wrapper of home icon and CHATS */
+//   .ant-card-body {
+//     padding: 0;
+//     margin: 0;
+//     border: none;
+//     background-color: grey;
+//     /* display: flex; */
+//     /* justify-content: center; */
+//   }
+
+//   /* top left home icon and CHATS */
+//   .jIRBJL {
+//     padding: 0.5rem;
+//     margin: 0;
+//     border: none;
+//     background-color: purple;
+//   }
+
+//   /* CHATS */
+//   .bEKYEe {
+//     display: none;
+//   }
+
+//   .iQMAjv {
+//     background-color: orange;
+//     margin: 0;
+//     width: 100%;
+//   }
+
+//   #chatBox {
+//     background-color: yellow;
+//     width: 50%;
+//   }
+// }
